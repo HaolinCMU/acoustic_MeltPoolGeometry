f6881839632440ebad4c579ad001a5fbe0965a11
HaolinCMU <haolinl@andrew.cmu.edu>
2021-08-30 21:35:21 -0400

    Add files via upload

--- resultAnalysis.py
+++ resultAnalysis.py
@@ -9,6 +9,7 @@ import copy
 import os
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.cm as cm
 import scipy.io
 import shutil
 import multiprocessing
@@ -412,6 +413,79 @@ def raw_visualization_plot(data, label_array, n_features, figure_name="raw_visua
         ax.view_init(-140, 30)
     
     plt.savefig(figure_name)
+
+
+def evaluateClusterGoodness(data, label_array):
+    """
+    """
+
+    silhouette_avg = skc.silhouette_score(data, label_array)
+    sample_silhouette_values = skc.silhouette_samples(data, label_array)
+
+    return silhouette_avg, sample_silhouette_values
+
+
+def clusterGoodness_Visualization(data, label_array, center_array, n_clusters, silhouette_avg, 
+                                  sample_silhouette_values):
+    """
+    """
+
+    fig, (ax1, ax2) = plt.subplots(1, 2)
+    fig.set_size_inches(18, 7)
+    ax1.set_xlim([-0.1, 1])
+    ax1.set_ylim([0, len(data) + (n_clusters + 1) * 10])
+
+    silhouette_avg, sample_silhouette_values = evaluateClusterGoodness(data, label_array)
+
+    y_lower = 10
+    for i in range(n_clusters):
+        ith_cluster_silhouette_values = \
+            sample_silhouette_values[label_array == i]
+
+        ith_cluster_silhouette_values.sort()
+
+        size_cluster_i = ith_cluster_silhouette_values.shape[0]
+        y_upper = y_lower + size_cluster_i
+
+        color = cm.nipy_spectral(float(i) / n_clusters)
+        ax1.fill_betweenx(np.arange(y_lower, y_upper),
+                          0, ith_cluster_silhouette_values,
+                          facecolor=color, edgecolor=color, alpha=0.7)
+
+        ax1.text(-0.05, y_lower + 0.5 * size_cluster_i, str(i))
+
+        y_lower = y_upper + 10
+
+    ax1.set_title("The silhouette plot for the various clusters.")
+    ax1.set_xlabel("The silhouette coefficient values")
+    ax1.set_ylabel("Cluster label")
+
+    # The vertical line for average silhouette score of all the values
+    ax1.axvline(x=silhouette_avg, color="red", linestyle="--")
+
+    ax1.set_yticks([])  # Clear the yaxis labels / ticks
+    ax1.set_xticks([-0.1, 0, 0.2, 0.4, 0.6, 0.8, 1])
+
+    # 2nd Plot showing the actual clusters formed
+    colors = cm.nipy_spectral(label_array.astype(float) / n_clusters)
+    ax2.scatter(data[:, 0], data[:, 1], marker='.', s=30, lw=0, alpha=0.7,
+                c=colors, edgecolor='k')
+
+    # Draw white circles at cluster centers
+    ax2.scatter(center_array[:, 0], center_array[:, 1], marker='o',
+                c="white", alpha=1, s=200, edgecolor='k')
+
+    for i, c in enumerate(center_array):
+        ax2.scatter(c[0], c[1], marker='$%d$' % i, alpha=1,
+                    s=50, edgecolor='k')
+
+    ax2.set_title("The visualization of the clustered data.")
+    ax2.set_xlabel("Feature space for the 1st feature")
+    ax2.set_ylabel("Feature space for the 2nd feature")
+
+    plt.suptitle(("Silhouette analysis for KMeans clustering on sample data "
+                  "with n_clusters = %d" % n_clusters),
+                  fontsize=14, fontweight='bold')
 
 
 if __name__ == "__main__":
